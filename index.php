fb5491ec38b88d8f597e035ebeaa56483a4fdc02
g-kamal123 <104122283+g-kamal123@users.noreply.github.com>
2022-05-10 13:00:50 +0530

    Write a PHP script to delete a specific value from an array using array_filter() function.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-echo "<h2>Write a PHP program to filter out some elements with certain key-names.</h2>";
-$arr1 = array('c1' => 'Red', 'c2' => 'Green', 'c3' => 'White', 'c4' => 'Black');
-$arr2 = array('c2', 'c4');
-$result = array_diff_key($arr1,array_flip($arr2));
-print_r($result);
+  echo "<h2>Write a PHP program to filter out some elements with certain key-names.</h2>";
+  $arr1 = [1,45,45,5,33,6];
+  print_r(array_filter($arr1,"test"));
+  function test($val){
+      return $val-45;
+  }
+
 ?>
